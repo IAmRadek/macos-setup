b1eaf399f933fb7b5a2d41e9b4715606f3218a97
Radosław Dejnek <radek@dejnek.pl>
2025-08-15 09:50:03 +0200

    feat: ssh setup

--- configuration.nix
+++ configuration.nix
@@ -135,7 +135,6 @@
 
   # SSH configuration with 1Password agent
   programs.ssh = {
-    enable = true;
     extraConfig = ''
       Host *
         IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"

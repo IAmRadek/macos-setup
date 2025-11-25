d4722a21abdb832683ad140d03d7b5a658ead133
Radosław Dejnek <radek@dejnek.pl>
2025-11-25 22:15:28 +0100

    feat: colima-tools

--- modules/tools.nix
+++ modules/tools.nix
@@ -32,17 +32,43 @@ let
 in
 {
   home.packages = [
-    (pkgs.writeShellScriptBin "colima-tools" ''
-      set -euo pipefail
-      exec ${py}/bin/inv -r ''$HOME/.nix-darwin/tools/colima "''${1}" -- "''${@:2}"
-    '')
+    git-pr
+    (pkgs.writeShellScriptBin "colima-tools" (builtins.readFile ../tools/colima/colima-tools.sh))
   ];
 
-  home.file.".zsh/completions/_colima-tools".source = ../tools/_colima;
+  home.file.".zsh/completions/_colima-tools".text = ''
+    # Completions for colima-tools
+    _colima_tools_complete() {
+        local cur="''${COMP_WORDS[COMP_CWORD]}"
+        local cmd="''${COMP_WORDS[1]:-}"
+
+        if [[ $COMP_CWORD -eq 1 ]]; then
+            COMPREPLY=( $(compgen -W "help start stop restart status logs prune nuke ssh images ports completions" -- "$cur") )
+            return
+        fi
+
+        case "$cmd" in
+            start)
+                COMPREPLY=( $(compgen -W "--profile --cpu --mem --disk --vm --k8s --gpu --arch --dns --mirror --help" -- "$cur") )
+                ;;
+            stop|restart|status|logs|ssh|ports)
+                COMPREPLY=( $(compgen -W "--profile --help" -- "$cur") )
+                ;;
+            nuke)
+                COMPREPLY=( $(compgen -W "--profile --yes --help" -- "$cur") )
+                ;;
+            prune|images|help|completions)
+                COMPREPLY=( $(compgen -W "--help" -- "$cur") )
+                ;;
+        esac
+    }
+
+    complete -F _colima_tools_complete colima-tools
+  '';
 
   home.file.".runbooks/new.sh".source = ../tools/runbooks/new.sh;
   home.activation.mySymlinks = lib.mkAfter ''
-   	  ln -sf ~/.nix-darwin/tools/navi/cheats ~/.local/share/navi/cheats/local
-   	'';
+    	  ln -sf ~/.nix-darwin/tools/navi/cheats ~/.local/share/navi/cheats/local
+    	'';
 
 }
